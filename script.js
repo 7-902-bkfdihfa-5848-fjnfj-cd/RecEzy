0fb8a1b710704b197d0306373ab1284ea335d196
7-902-bkfdihfa-5848-fjnfj-cd <alexandru.barza603@gmail.com>
2024-08-24 21:16:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+// Scroll event listener to change header background and add shadow on scroll
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('.header');
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
+// ScrollReveal animations
 ScrollReveal({
     reset: false,
     distance: '80px',
     duration: 2000,
     delay: 200
-})
+});
 
-// .img-slider
-ScrollReveal().reveal('.logo, .imagelogo' , { origin: 'top' });
+ScrollReveal().reveal('' , { origin: 'top' });
 ScrollReveal().reveal('.image img, .image2 img, .image3 img, .glowing-circle', { origin: 'bottom' }); 
 ScrollReveal().reveal('.home-sci ', { origin: 'left' }); 
-ScrollReveal().reveal('.navbar a, .home-content p, .home-content h1, .btn', { origin: 'right' }); 
+ScrollReveal().reveal('.home-content p, .home-content h1, .btn', { origin: 'right' }); 
