f11437e144a51f1f4255bd89bcc5442cc60c5d5f
7-902-bkfdihfa-5848-fjnfj-cd <alexandru.barza603@gmail.com>
2024-09-14 21:25:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('.header');
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
+const menuIcon = document.getElementById('menu-icon');
+const navbar = document.querySelector('.navbar');
+const menuOverlay = document.getElementById('menu-overlay');
+const menuIconList = menuIcon.querySelector('.bx-list-ul');
+const menuIconX = menuIcon.querySelector('.bx-x');
+
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+    menuOverlay.classList.toggle('active');
+    menuIcon.classList.toggle('active');
+    menuIconList.classList.toggle('hidden');
+    menuIconX.classList.toggle('hidden');
+});
+
+menuOverlay.addEventListener('click', () => {
+    navbar.classList.remove('active');
+    menuOverlay.classList.remove('active');
+    menuIcon.classList.remove('active');
+    menuIconList.classList.remove('hidden');
+    menuIconX.classList.remove('hidden');
+});
+
+let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('animate');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 0.1 });
